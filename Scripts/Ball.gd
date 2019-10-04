9eaafd4d4e5887d92d8dc9ff88c71480011c8e90
Md Sahil <287mdsahil@gmail.com>
2019-10-05 02:21:06 +0530

    added brick destruction and ball tragectory control

---
+++ Scripts/Ball.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+
+# warning-ignore:unused_argument
+func _process(delta):
+	var bodies = get_colliding_bodies()
+	for body in bodies:
+		if(body.is_in_group("Brick")):
+			body.queue_free()
+		elif(body.get_name() == "Paddle"):
+			var speed = linear_velocity.length()
+			print(get_global_position())
+			print(body.get_node("Anchor").get_global_position())
+			var dir = get_global_position() - body.get_node("Anchor").get_global_position()
+			dir = dir.normalized()
+			var velocity = dir*300
+			linear_velocity = velocity
